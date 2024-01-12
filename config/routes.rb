7808d9752b2472ebc09d7c0f6dde8d4752ee53f6
Alex Blom <alexblom1989@gmail.com>
2024-01-12 17:56:31 -0500

    email confirmation view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'static_pages/admin'
   root "static_pages#home"
   post "sign_up", to: "users#create"
   get "sign_up", to: "users#new"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
