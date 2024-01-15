b4232bf287a2e90e8dfb4dea32a7a41067807415
Alex Blom <alexblom1989@gmail.com>
2024-01-14 23:35:14 -0500

    Generate Sessions controller, add new.html.erb, add routes for login.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   root "static_pages#home"
   post "sign_up", to: "users#create"
   get "sign_up", to: "users#new"
+  
   resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
   
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  get "login", to: "sessions#new"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
