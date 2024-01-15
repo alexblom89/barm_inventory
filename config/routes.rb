1cb1729662a5e5757fd794b6a6b0390809782c4c
Alex Blom <alexblom1989@gmail.com>
2024-01-15 00:25:03 -0500

    Add passwords route. Add new and edit password views.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   root "static_pages#home"
   post "sign_up", to: "users#create"
   get "sign_up", to: "users#new"
-  
+
   resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
+  resources :passwords, only: [:create, :edit, :new, :update], param: :password_reset_token
+
   
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
