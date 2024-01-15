95a3e379e79e5de62d641d94e2636f29dcbdd238
Alex Blom <alexblom1989@gmail.com>
2024-01-15 01:28:20 -0500

    Add remember_me logic to sessions controller, add remember_me checkbox to sessions.new view.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
     before_action :redirect_if_authenticated, only: [:create, :new]
+    before_action :authenticate_user!, only: [:destroy]
   
     def create
       @user = User.find_by(email: params[:user][:email].downcase)
@@ -8,6 +9,7 @@ class SessionsController < ApplicationController
           redirect_to new_confirmation_path, alert: "Incorrect email or password."
         elsif @user.authenticate(params[:user][:password])
           login @user
+          remember(@user) if params[:user][:remember_me] == "1"
           redirect_to root_path, notice: "Signed in."
         else
           flash.now[:alert] = "Incorrect email or password."
@@ -21,6 +23,7 @@ class SessionsController < ApplicationController
   
     def destroy
       logout
+      forget(current_user)
       redirect_to root_path, notice: "Signed out."
     end
   
