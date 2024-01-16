c560b8c3b33aa68707a10a7da74e90cf790aa33b
Alex Blom <alexblom1989@gmail.com>
2024-01-15 23:08:26 -0500

    Add active sessions controller to destroy sessions. Add routes and pages to support this. Update partial.

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,12 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
   get "login", to: "sessions#new"
 
+  resources :active_sessions, only: [:destroy] do
+    collection do
+      delete "destroy_all"
+    end
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
